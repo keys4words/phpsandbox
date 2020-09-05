6840bf9289a952b408fb1fae4644e4dddecf7ebb
keys4words <keys4words@gmail.com>
2020-09-05 19:20:50 +0300

    Add variables & consts

---
+++ index.php
@@ -0,0 +1,18 @@
+<h1>
+<?php
+
+    // variables and consts
+    $str1 = 'shit';
+    $str2 = 'happens';
+    echo $str1.' '.$str2;
+    $sum = "$str1 $str2";
+    echo $sum;
+    define('GREETING', 'hey!');
+    // GREETING = 'hello, dude!';
+    echo '<br>'.GREETING;
+    
+    // data types
+
+
+?>
+</h1>
