bc15871e5386e3da5f57e40afe81ec2fcf89a7fe
keys4words <keys4words@gmail.com>
2020-09-07 16:32:20 +0300

    change folder structure & add include

---
+++ website01/content.php
@@ -0,0 +1,3 @@
+<?php require_once('inc/header.php'); ?>
+    <h1>Content</h1>
+<?php include('inc/footer.php'); ?>
---
+++ website01/inc/footer.php
@@ -0,0 +1,6 @@
+
+    <footer>
+        <p>MyWebsite &copy: 2020</p>
+    </footer>
+</body>
+</html>
